cda277e50b6e7b7fd7f29041e9abab0d3f0130c7
PriyangaSankar <priyangasankar2003@gmail.com>
2024-01-22 14:45:29 +0530

    Final template added

--- taskmanagement/src/app/app-routing.module.ts
+++ taskmanagement/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  // { path: 'users', component: UsersComponent },
+  // { path: 'projects', component: ProjectsComponent },
+  // { path: 'tasks', component: TasksComponent },
+  // { path: 'teams', component: TeamsComponent },
+  { path: '', component: HomeComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
