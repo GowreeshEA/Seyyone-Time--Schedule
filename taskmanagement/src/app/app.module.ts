570752b3ad1c85e054d54a9b5f52b6e80816f272
Lakshmipriya-18 <lp476340@gmail.com>
2024-01-23 10:41:04 +0530

    Team c added

--- taskmanagement/src/app/app.module.ts
+++ taskmanagement/src/app/app.module.ts
@@ -11,6 +11,13 @@ import { ProjectComponent } from './project/project.component';
 import { TaskComponent } from './task/task.component';
 import { ChartsComponent } from './charts/charts.component';
 import { ViewtaskComponent } from './viewtask/viewtask.component';
+import { AuditComponent } from './Team c/audit/audit.component';
+import { DocumentComponent } from './document/document.component';
+import { TeamsComponent } from './teams/teams.component';
+import { TimetrackerComponent } from './timetracker/timetracker.component';
+import { BoardsComponent } from './boards/boards.component';
+import { ImageuploadComponent } from './imageupload/imageupload.component';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +27,14 @@ import { ViewtaskComponent } from './viewtask/viewtask.component';
     ProjectComponent,
     TaskComponent,
     ChartsComponent,
-    ViewtaskComponent
+    ViewtaskComponent,
+    AuditComponent,
+    DocumentComponent,
+    TeamsComponent,
+    TimetrackerComponent,
+    BoardsComponent,
+    ImageuploadComponent,
+    NotificationComponent
   ],
   imports: [
     NgbModalModule,
